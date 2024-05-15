507bdc7d5b3aeb34209d42c3ac23ab62c0b9b1d8
Paulo Vieira <Paulo.Vieira@ctw.bmwgroup.com>
2024-05-15 19:36:29 +0100

    added clerk auth

---
+++ app/(platform)/layout.tsx
@@ -0,0 +1,15 @@
+import { ClerkProvider } from "@clerk/nextjs";
+
+interface Props {
+    children: React.ReactNode;
+}
+
+const PlatformLayout = ({children}: Props) => {
+    return (
+        <ClerkProvider>
+            {children}
+        </ClerkProvider>
+    )
+}
+
+export default PlatformLayout;
