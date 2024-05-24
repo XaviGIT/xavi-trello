1a1de99eb4e0f84725940668f771c26514837b2a
Paulo Vieira <Paulo.Vieira@ctw.bmwgroup.com>
2024-05-24 11:41:38 +0100

    added card modal

--- components/modals/card-modal/card-modal.tsx
+++ components/modals/card-modal/card-modal.tsx
@@ -2,11 +2,23 @@
 
 import { Dialog, DialogContent } from "@/components/ui/dialog";
 import { useCardModal } from "@/hooks/use-card-modal";
+import { fetcher } from "@/lib/fetcher";
+import { CardWithList } from "@/types";
+import { useQuery } from "@tanstack/react-query"
+import { Header } from "./header";
 
 export const CardModal = () => {
+
+
+
     const id = useCardModal((state) => state.id);
     const isOpen = useCardModal((state) => state.isOpen);
     const onClose = useCardModal((state) => state.onClose);
+
+    const { data: cardData } = useQuery<CardWithList>({
+        queryKey: ["card", id],
+        queryFn: () => fetcher(`/api/cards/${id}`)
+    });
 
     return (
         <Dialog
@@ -14,7 +26,13 @@ export const CardModal = () => {
             onOpenChange={onClose}
         >
             <DialogContent>
-                i'm a modal
+            {
+                !cardData ? (
+                    <Header.Skeleton />
+                ) : (
+                    <Header data={cardData} />
+                )
+            }
             </DialogContent>
         </Dialog>
     )
---
+++ components/providers/query-provider.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { useState } from "react";
+
+interface Props {
+    children: React.ReactNode;
+}
+
+export const QueryProvider = ({ children }: Props) => {
+    const [queryClient, setQueryClient] = useState(() => new QueryClient())
+
+    return (
+        <QueryClientProvider client={queryClient}>
+            {children}
+        </QueryClientProvider>
+    )
+}
